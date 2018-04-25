d874cdd80dda03c4d4b5cdb26121b0d2de695bdb
David Fletcher <dfletch1@binghamton.edu>
2018-04-25 18:29:27 -0400

    Started FullAssembler and passed the file in

---
+++ workspace/CS140FinalProject/src/project/FullAssembler.java
@@ -0,0 +1,11 @@
+package project;
+
+public class FullAssembler implements Assembler {
+
+	@Override
+	public int assembler(String inputFileName, String outputFileName, StringBuilder error) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
