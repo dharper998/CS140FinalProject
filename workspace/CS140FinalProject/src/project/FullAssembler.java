90c40d14431ae79a1506b68ffb0c4b516643275c
David Fletcher <dfletch1@binghamton.edu>
2018-04-25 19:07:27 -0400

    FullAssembler should ACTUALLY read the file now

--- workspace/CS140FinalProject/src/project/FullAssembler.java
+++ workspace/CS140FinalProject/src/project/FullAssembler.java
@@ -1,19 +1,19 @@
 package project;
 
 import java.util.Scanner;
-import java.util.List;
 import java.util.ArrayList;
+
 
 public class FullAssembler implements Assembler {
 
 	@Override
-	public int assembler(String inputFileName, String outputFileName, StringBuilder error) {
-		Scanner reader = new Scanner(System.in);
+	public int assemble(String inputFileName, String outputFileName, StringBuilder error) {
 		ArrayList<String> fileIn = new ArrayList<String>();
-		while(reader.hasNext()) {
-			fileIn.add(reader.nextLine());
+		try(Scanner reader = new Scanner(inputFileName)){
+			while(reader.hasNext()) {
+				fileIn.add(reader.nextLine());
+			}
 		}
 		return 0;
 	}
-
 }
