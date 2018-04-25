735170f79e8e4e14dec17f35598554a844dc7b0b
Drew Harper <dharper998@gmail.com>
2018-04-25 12:37:29 -0400

    Started Part 1

---
+++ workspace/CS140FinalProject/src/project/MachineModel.java
@@ -0,0 +1,66 @@
+package project;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MachineModel {
+	private class CPU {
+		private int accumulator;
+		private int instructionPointer;
+		private int memoryBase;
+		
+		public void incrementIP(int val) {
+			instructionPointer += val;
+		}
+	}
+	
+	public final Map<Integer, Instruction> INSTRUCTIONS = new TreeMap<Integer, Instruction>();
+	private CPU cpu = new CPU();
+	private Memory memory = new Memory();
+	private HaltCallback callback;
+	private boolean withGUI;
+	
+	public MachineModel() {
+		this(false, null);
+	}
+
+	public MachineModel(boolean b, HaltCallback hcb) {
+		withGUI = b;
+		callback = hcb;
+		
+		 //INSTRUCTION_MAP entry for "ADDI"
+        INSTRUCTIONS.put(0xC, arg -> {
+            cpu.accumulator += arg;
+            cpu.incrementIP(1);
+        });
+
+        //INSTRUCTION_MAP entry for "ADD"
+        INSTRUCTIONS.put(0xD, arg -> {
+            int arg1 = memory.getData(cpu.memoryBase+arg);
+            cpu.accumulator += arg1;
+            cpu.incrementIP(1);
+        });
+
+        //INSTRUCTION_MAP entry for "ADDN"
+        INSTRUCTIONS.put(0xE, arg -> {
+            int arg1 = memory.getData(cpu.memoryBase+arg);
+            int arg2 = memory.getData(cpu.memoryBase+arg1);
+            cpu.accumulator += arg2;
+            cpu.incrementIP(1);
+        });
+        
+        //INSTRUCTION_MAP entry for "SUBI"
+        INSTRUCTIONS.put(0xF, arg -> {
+            cpu.accumulator -= arg;
+            cpu.incrementIP(1);
+        });
+        
+        //INSTRUCTION_MAP entry for "SUB"
+        INSTRUCTIONS.put(0x10, arg -> {
+            int arg1 = memory.getData(cpu.memoryBase+arg);
+            cpu.accumulator += arg1;
+            cpu.incrementIP(1);
+        });
+	}
+
+}
