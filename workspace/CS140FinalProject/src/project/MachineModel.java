7af18021f2bd29545682a34dfc39af69683726c1
Drew Harper <dharper998@gmail.com>
2018-04-25 13:00:45 -0400

    Finished Instructions 0x0 - 0x17

--- workspace/CS140FinalProject/src/project/MachineModel.java
+++ workspace/CS140FinalProject/src/project/MachineModel.java
@@ -28,7 +28,90 @@ public class MachineModel {
 		withGUI = b;
 		callback = hcb;
 		
-		 //INSTRUCTION_MAP entry for "ADDI"
+		//INSTRUCTION_MAP entry for "NOP"
+		INSTRUCTIONS.put(0x0, arg -> {
+			cpu.incrementIP(1);
+		});
+		
+		//INSTRUCTION_MAP entry for "LODI"
+		INSTRUCTIONS.put(0x1, arg -> {
+			cpu.accumulator = arg;
+			cpu.incrementIP(1);
+		});
+		
+		//INSTRUCTION_MAP entry for "LOD"
+		INSTRUCTIONS.put(0x2, arg -> {
+			int arg1 = memory.getData(cpu.memoryBase + arg);
+			cpu.accumulator = arg1;
+			cpu.incrementIP(1);
+		});
+		
+		//INSTRUCTION_MAP entry for "LODN"
+		INSTRUCTIONS.put(0x3, arg -> {
+			int arg1 = memory.getData(cpu.memoryBase+arg);
+            int arg2 = memory.getData(cpu.memoryBase+arg1);
+            cpu.accumulator = arg2;
+            cpu.incrementIP(1);
+		});
+		
+		//INSTRUCTION_MAP entry for "STO"
+		INSTRUCTIONS.put(0x4, arg -> {
+			memory.setData(cpu.memoryBase + arg, cpu.accumulator);
+			cpu.incrementIP(1);
+		});
+		
+		//INSTRUCTION_MAP entry for "STON"
+		INSTRUCTIONS.put(0x5, arg -> {
+			int val = memory.getData(cpu.memoryBase+arg);
+			memory.setData(cpu.memoryBase + val, cpu.accumulator);
+			cpu.incrementIP(1);
+		});
+		
+		//INSTRUCTION_MAP entry for "JMPR"
+		INSTRUCTIONS.put(0x6, arg -> {
+			cpu.instructionPointer += arg;
+		});
+		
+		//INSTRUCTION_MAP entry for "JUMP"
+		INSTRUCTIONS.put(0x7, arg -> {
+			int val = memory.getData(cpu.memoryBase + arg);
+			cpu.instructionPointer += val;
+		});
+		
+		//INSTRUCTION_MAP entry for "JUMPI"
+		INSTRUCTIONS.put(0x8, arg -> {
+			cpu.instructionPointer = arg;
+		});
+		
+		//INSTRUCTION_MAP entry for "JMPZR"
+		INSTRUCTIONS.put(0x9, arg -> {
+			if(cpu.accumulator == 0) {
+				cpu.instructionPointer += arg;
+			} else {
+				cpu.incrementIP(1);
+			}
+		});
+		
+		//INSTRUCTION_MAP entry for "JMPZ"
+		INSTRUCTIONS.put(0xA, arg -> {
+			if(cpu.accumulator == 0) {
+				int val = memory.getData(cpu.memoryBase + arg);
+				cpu.instructionPointer += val;
+			} else {
+				cpu.incrementIP(1);
+			}
+		});
+		
+		//INSTRUCTION_MAP entry for "JMPZI"
+		INSTRUCTIONS.put(0xB, arg -> {
+			if(cpu.accumulator == 0) {
+				cpu.instructionPointer = arg;
+			} else {
+				cpu.incrementIP(1);
+			}
+		});
+		
+		//INSTRUCTION_MAP entry for "ADDI"
         INSTRUCTIONS.put(0xC, arg -> {
             cpu.accumulator += arg;
             cpu.incrementIP(1);
@@ -61,6 +144,58 @@ public class MachineModel {
             cpu.accumulator += arg1;
             cpu.incrementIP(1);
         });
+        
+        //INSTRUCTION_MAP entry for "SUBN"
+        INSTRUCTIONS.put(0x11, arg -> {
+            int arg1 = memory.getData(cpu.memoryBase+arg);
+            int arg2 = memory.getData(cpu.memoryBase+arg1);
+            cpu.accumulator -= arg2;
+            cpu.incrementIP(1);
+        });
+        
+        //INSTRUCTION_MAP entry for "MULI"
+        INSTRUCTIONS.put(0x12, arg -> {
+            cpu.accumulator *= arg;
+            cpu.incrementIP(1);
+        });
+        
+        //INSTRUCTION_MAP entry for "MUL"
+        INSTRUCTIONS.put(0x13, arg -> {
+            int arg1 = memory.getData(cpu.memoryBase+arg);
+            cpu.accumulator *= arg1;
+            cpu.incrementIP(1);
+        });
+        
+        //INSTRUCTION_MAP entry for "MULN"
+        INSTRUCTIONS.put(0x14, arg -> {
+            int arg1 = memory.getData(cpu.memoryBase+arg);
+            int arg2 = memory.getData(cpu.memoryBase+arg1);
+            cpu.accumulator *= arg2;
+            cpu.incrementIP(1);
+        });
+        
+        //INSTRUCTION_MAP entry for "DIVI"
+        INSTRUCTIONS.put(0x15, arg -> {
+            cpu.accumulator /= arg;
+            cpu.incrementIP(1);
+        });
+        
+        //INSTRUCTION_MAP entry for "DIV"
+        INSTRUCTIONS.put(0x16, arg -> {
+            int arg1 = memory.getData(cpu.memoryBase+arg);
+            cpu.accumulator /= arg1;
+            cpu.incrementIP(1);
+        });
+        
+        //INSTRUCTION_MAP entry for "DIVN"
+        INSTRUCTIONS.put(0x17, arg -> {
+            int arg1 = memory.getData(cpu.memoryBase+arg);
+            int arg2 = memory.getData(cpu.memoryBase+arg1);
+            cpu.accumulator /= arg2;
+            cpu.incrementIP(1);
+        });
+        
+        
 	}
 
 }
