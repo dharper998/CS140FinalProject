08afe955e04f4a64d40238a4a2b80d664c9bbcfe
David Fletcher <dfletch1@binghamton.edu>
2018-04-25 18:38:08 -0400

    Fix

--- workspace/CS140FinalProject/src/project/FullAssembler.java
+++ workspace/CS140FinalProject/src/project/FullAssembler.java
@@ -1,10 +1,18 @@
 package project;
 
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class FullAssembler implements Assembler {
 
 	@Override
 	public int assembler(String inputFileName, String outputFileName, StringBuilder error) {
-		// TODO Auto-generated method stub
+		Scanner reader = new Scanner(System.in);
+		ArrayList<String> fileIn = new ArrayList<String>();
+		while(reader.hasNext()) {
+			fileIn.add(reader.nextLine());
+		}
 		return 0;
 	}
 
