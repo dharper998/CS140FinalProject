f50468ae91149a77b8b13d4d3ae834e2075ae131
Drew Harper <dharper998@gmail.com>
2018-04-26 13:03:09 -0400

    Working on Full Assembler

--- workspace/CS140FinalProject/src/project/FullAssembler.java
+++ workspace/CS140FinalProject/src/project/FullAssembler.java
@@ -8,12 +8,37 @@ public class FullAssembler implements Assembler {
 
 	@Override
 	public int assemble(String inputFileName, String outputFileName, StringBuilder error) {
+		
+		if(error == null) {
+			throw new IllegalArgumentException("Coding error: the error buffer is null");
+		}
+		
 		ArrayList<String> fileIn = new ArrayList<String>();
 		try(Scanner reader = new Scanner(inputFileName)){
 			while(reader.hasNext()) {
 				fileIn.add(reader.nextLine());
 			}
 		}
+		
+		int lineNum = 0;
+		boolean blankLineFound = false;
+		for(String line : fileIn) {
+			if(line.trim().length() == 0 && blankLineFound == false) {
+				blankLineFound = true;
+				int errorLine = lineNum;
+			} else if(line.trim().length() != 0 && blankLineFound == true) {
+				error.append("\nIllegal blank line in the source file");
+			}
+			
+			if(line.charAt(0) == ' ' || line.charAt(0) == '\t') {
+				error.append("\nLine starts with illegal white space");
+			}
+			
+			if(line.trim().toUpperCase().equals("DATA") && readingCode == true)
+			
+			lineNum++;
+		}
+		
 		return 0;
 	}
 }
