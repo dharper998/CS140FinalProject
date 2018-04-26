fc09efabfd668e8a7a44a9513372c7ad70bdc60e
Alison Garrity <nosila98@gmail.com>
2018-04-26 11:49:19 -0400

    projectView package

---
+++ workspace/CS140FinalProject/src/projectView/ViewMediator.java
@@ -0,0 +1,17 @@
+package projectView;
+
+import project.MachineModel;
+
+public class ViewMediator /*extends Observable*/ {
+	private MachineModel model;
+	
+	public MachineModel getModel() {
+		return model;
+	}
+
+	public void setModel(MachineModel model) {
+		this.model = model;
+	}
+
+	public void step() {}
+}
