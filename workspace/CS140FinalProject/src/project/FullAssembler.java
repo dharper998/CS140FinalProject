201a681830552776ef180f00b804358a0d076f83
Drew Harper <31670344+dharper998@users.noreply.github.com>
2018-04-26 15:08:15 -0400

    Progress on Full Assembler

--- workspace/CS140FinalProject/src/project/FullAssembler.java
+++ workspace/CS140FinalProject/src/project/FullAssembler.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 
 
 public class FullAssembler implements Assembler {
+	private boolean readingCode = true;
 
 	@Override
 	public int assemble(String inputFileName, String outputFileName, StringBuilder error) {
-		
+
 		if(error == null) {
 			throw new IllegalArgumentException("Coding error: the error buffer is null");
 		}
@@ -20,25 +21,40 @@ public class FullAssembler implements Assembler {
 			}
 		}
 		
+		int errorLine;
 		int lineNum = 0;
 		boolean blankLineFound = false;
 		for(String line : fileIn) {
+			//ERROR 1
 			if(line.trim().length() == 0 && blankLineFound == false) {
 				blankLineFound = true;
-				int errorLine = lineNum;
+				errorLine = lineNum;
 			} else if(line.trim().length() != 0 && blankLineFound == true) {
 				error.append("\nIllegal blank line in the source file");
+				errorLine = lineNum;
 			}
 			
+			//ERROR 2
 			if(line.charAt(0) == ' ' || line.charAt(0) == '\t') {
 				error.append("\nLine starts with illegal white space");
+				errorLine = lineNum;
 			}
 			
-			if(line.trim().toUpperCase().equals("DATA") && readingCode == true)
+			//ERROR 3
+			if(line.trim().toUpperCase().equals("DATA") && readingCode == true) {
+				if(!line.trim().equals("DATA")) {
+					error.append("\nLine does not have DATA in upper case");
+					errorLine = lineNum;
+				}
+			} else if(line.trim().equals("DATA") && readingCode == false) {
+				error.append("\nSecond separator found");
+			}
+			
+			
 			
 			lineNum++;
 		}
 		
-		return 0;
+		return errorLine;
 	}
 }
