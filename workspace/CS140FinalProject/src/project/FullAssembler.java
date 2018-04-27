79da95845b00bd3e8bfb56af35921e8221459afd
Drew Harper <31670344+dharper998@users.noreply.github.com>
2018-04-27 12:51:50 -0400

    Almost completed Full Assembler

--- workspace/CS140FinalProject/src/project/FullAssembler.java
+++ workspace/CS140FinalProject/src/project/FullAssembler.java
@@ -34,7 +34,6 @@ public class FullAssembler implements Assembler {
 			//ERROR 1
 			if(line.trim().length() == 0 && blankLineFound == false) {
 				blankLineFound = true;
-				errorLine = lineNum;
 			} else if(line.trim().length() != 0 && blankLineFound == true) {
 				error.append("\nIllegal blank line in the source file");
 				errorLine = lineNum;
@@ -51,6 +50,9 @@ public class FullAssembler implements Assembler {
 				if(!line.trim().equals("DATA")) {
 					error.append("\nLine does not have DATA in upper case");
 					errorLine = lineNum;
+				} else {
+					readingCode = false;
+					continue;
 				}
 			} else if(line.trim().equals("DATA") && readingCode == false) {
 				error.append("\nSecond separator found");
@@ -60,32 +62,49 @@ public class FullAssembler implements Assembler {
 			if(readingCode == true) {
 				String[] parts = line.trim().split("\\s+");
 				if(!InstrMap.toCode.keySet().contains(parts[0])) {
-					error.append("\nError on line " + (errorLine+1) + ": illegal mnemonic");
+					error.append("\nError on line " + (lineNum+1) + ": illegal mnemonic");
 					errorLine = lineNum;
 				} else {
 					if(!parts[0].equals(parts[0].toUpperCase())) {
-						error.append("\nError on line" + (errorLine+1) + ": illegal mnemonic");
+						error.append("\nError on line" + (lineNum+1) + ": illegal mnemonic");
 					} else {
 						//ERROR 5
 						if(noArgument.contains(parts[0])) {
-							if(parts[0].length() != 1) {
-								error.append("\nError on line " + (errorLine+1) + ": this mnemonic cannot take arguments");
+							if(parts.length != 1) {
+								error.append("\nError on line " + (lineNum+1) + ": this mnemonic cannot take arguments");
 							}
 						} else {
-							if(parts[0].length() > 2) {
-								error.append("\nError on line " + (errorLine+1) + ": this mnemonic has too many arguments");
-							} else if(parts[0].length() < 2) {
-								error.append("\nError on line " + (errorLine+1) + ": this mnemonic is missing an argument");
+							if(parts.length > 2) {
+								error.append("\nError on line " + (lineNum+1) + ": this mnemonic has too many arguments");
+							} else if(parts.length < 2) {
+								error.append("\nError on line " + (lineNum+1) + ": this mnemonic is missing an argument");
 							}
 							//ERROR 6
 							try{
 								int arg = Integer.parseInt(parts[1],16);
 							} catch(NumberFormatException e) {
-								error.append("\nError on line " + (errorLine+1) + ": argument is not a hex number");
+								error.append("\nError on line " + (lineNum+1) + ": argument is not a hex number");
 								errorLine = lineNum;	
 							}
 						}
 					}
+				}
+			} else {
+				String[] parts = line.trim().split("\\s+");
+				if(parts.length == 2) {
+					try {
+						int address = Integer.parseInt(parts[0], 16);
+						int value = Integer.parseInt(parts[1],16);
+					} catch(NumberFormatException e) {
+						error.append("\nError on line " + (lineNum+1) + ": data has non-numeric memory address");
+						errorLine = lineNum;
+					}
+				} else if(parts.length > 2){
+					error.append("\nError on line " + (lineNum+1) + ": data has too many arguments");
+					errorLine = lineNum;
+				} else {
+					error.append("\nError on line " + (lineNum+1) + ": data has too many arguments");
+					errorLine = lineNum;
 				}
 			}
 			
@@ -94,14 +113,19 @@ public class FullAssembler implements Assembler {
 			lineNum++;
 		}
 		
-		try (PrintWriter output = new PrintWriter(outputFileName)){
-			for(String s : outputCode) output.println(s);
-		} catch (FileNotFoundException e) {
-			error.append("\nError: Unable to write the assembled program to the output file");
-			errorLine = -1;
-		} catch (IOException e) {
-			error.append("\nUnexplained IO Exception");
-			errorLine = -1;
+		if(errorLine == 0) {
+			System.out.println("Enter the name of the file without extension: ");
+			try { 
+				int i = new SimpleAssembler().assemble(inputFileName + ".pasm", 
+						outputFileName + ".pexe", error);
+				System.out.println("result = " + i);
+			} catch (FileNotFoundException e) {
+				error.append("\nError: Unable to write the assembled program to the output file");
+				errorLine = -1;
+			} catch (IOException e) {
+				error.append("\nUnexplained IO Exception");
+				errorLine = -1;
+			}
 		}
 		
 		return errorLine;
